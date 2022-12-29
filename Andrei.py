fb7540b5f65bb4c7d567abcfd98051dcef2774cb
Alexey <Alexey.gt@bk.ru>
2022-12-29 19:57:13 +0300

    Dz

---
+++ Andrei.py
@@ -0,0 +1,105 @@
+import requests
+import datetime
+import logging
+from aiogram import Bot, Dispatcher, executor, types
+from config import TOKEN_API, WEATHER_API
+# import markups as nav
+import random
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+# Initialize bot and dispatcher
+bot = Bot(token=TOKEN_API)
+dp = Dispatcher(bot)
+
+
+# HELP_COMMAND = """
+# /start - начало работы с ботом
+# /help - список команд
+# /Weather - Погода
+# """
+
+
+
+
+async def on_startup(_):
+    print('Bot launched successfully!')
+
+
+@dp.message_handler(commands=['start'])
+async def send_welcome(message: types.Message):
+    """
+        This handler will be called when user sends `/start` command
+    """
+    await bot.send_message(message.from_user.id, 'Приветствую тебя! {0.first_name}'.format(message.from_user) +' 👋')
+    await message.delete()
+
+
+# @dp.message_handler(commands=['help'])
+# async def send_helper(message: types.Message):
+#     """
+#         This handler will be called when user sends  `/help` command
+#     """
+#     await message.reply(text=HELP_COMMAND)
+
+@dp.message_handler()
+async def choose(message: types.Message):
+
+    code_to_emoji = {
+        "Clear": "Ясно ☀️",
+        "Clouds": "Облачно ☁️",
+        "Rain": "Дождь ☔️",
+        "Drizzle": "Морось 🫧",
+        "Thunderstorm": "Гроза 🌩",
+        "Snow": "Снег ❄️",
+        "Mist": "Туман 🌫"
+    }
+
+    try:
+        r = requests.get(
+            f"https://api.openweathermap.org/data/2.5/weather?q={message.text}&appid={WEATHER_API}&units=metric"
+        )
+        data = r.json()
+
+        city = data["name"]
+        cur_weather = data["main"]["temp"]
+
+        weather_description = data["weather"][0]["main"]
+        if weather_description in code_to_emoji:
+            wd = code_to_emoji[weather_description]
+        else:
+            wd = "Посмотри в окно, не могу понять что там происходит"
+
+        humidity = data["main"]["humidity"]
+        pressure = data["main"]["pressure"]
+        temp_max = data["main"]["temp_max"]
+        temp_min = data["main"]["temp_min"]
+        wind = data["wind"]["speed"]
+        sunrise_timestamp = datetime.datetime.fromtimestamp(data["sys"]["sunrise"])
+        sunset_timestamp = datetime.datetime.fromtimestamp(data["sys"]["sunset"])
+        await message.answer(f"----{datetime.datetime.now().strftime('%Y-%m-%d %H:%M')}----\n"
+                            f"Прогноз погоды в местности: {city}\nС температурой воздуха: {int(cur_weather)}°C {wd}\nВлажностью воздуха: {humidity}%\nС давлением воздуха: {pressure}мм.рт.ст"
+                            f"\nМаксимальная температура воздуха: {int(temp_max)}°C\nМинимальная температура воздуха: {int(temp_min)}°C\n"
+                            f"Скорость ветра: {wind}м/с\nВосход солнца: {sunrise_timestamp}\nЗакат солнца: {sunset_timestamp}\n"
+                            f"Хорошего дня!🤗")
+
+    except:
+        # if message.text == '🔢 Random Number':
+        #     await bot.send_message(message.from_user.id, 'Your number is: ' + str(random.randint(0, 1001)))
+        # elif message.text == '🧭 Main Menu':
+        #     await bot.send_message(message.from_user.id, '🧭 Main Menu', reply_markup= nav.mainMenu)
+        # elif message.text == '🌤 Weather':
+        #     await bot.send_message(message.from_user.id, "Погода 🌤\nМожете ввести локацию которая вас интересует 🌅")
+        # elif message.text == '⏩ Other':
+        #     await bot.send_message(message.from_user.id, '⏩ Other', reply_markup= nav.otherMenu)
+        # elif message.text == '🤖 Info':
+        #     await bot.send_message(message.from_user.id, "🧬 Fear is stupid. I'm sorry I was affraid, 1960" )
+        # elif message.text == '🎵 Music':
+        #     await bot.send_message(message.from_user.id, '🎵 Music', reply_markup= nav.otherMenu)
+        #     await message.answer('<a href="https://music.yandex.ru">Listen to music</a>',parse_mode="HTML")
+        #     await message.answer('Приятного прослушивания' + '🫶 ')
+        # else:
+            await message.reply("Проверьте введенное название! 🍎")
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, on_startup=on_startup)
--- main.py
+++ main.py
@@ -1,47 +1,95 @@
 from aiogram import Bot, types, executor
-from config import TOKEN_API
+from config import TOKEN_API, WEATHER_API
 from aiogram.dispatcher import Dispatcher
 from aiogram.dispatcher.filters import Command
-from calc_complex import calc_complex_main
-from calc_fraction import calc_fraction_main
-from calc_log_reader import log_reader
-
+# from Other import number
+# from calc_complex import calc_complex_main
+# from calc_fraction import calc_fraction_main
+# from calc_log_reader import log_reader
+import requests
+import logging
+# markups as nav
+logging.basicConfig(level=logging.INFO)
+import datetime
 
 bot = Bot(TOKEN_API)
 dp = Dispatcher(bot)
 
+
 @dp.message_handler(commands=['start'])
 async def start_command(message: types.Message):
-    await message.reply("Привет!\nЯ пока что умею вычислять комплексные и вещественные числа!\n"
+    await message.reply("Привет!\nЯ пока что умею вычислять комплексные и вещественные числа и показывать погоду!\n"
                         "Для вывода списка комманд введите /help")
     await message.delete()
 
 
 @dp.message_handler(commands=['help'])
 async def help_command(message: types.Message):
-    await message.reply('''Калькулятор: /calculate\nЧто-нибудь еще потом: /Other ''')
+    await message.reply('''Калькулятор: /calculate\nПрогноз погоды: /Weather ''')
+#
+# @dp.message_handler(commands=['calculate'])
+# async def calc_command(message: types.Message):
+#     await message.reply(f'''Выберите вид калькуляторов:
+#       Комплексные числа: /complex вид выражения при вводе -> a+bj + c-dj
+#       Дробные числа: /fraction вид выражения при вводе -> a/b + c/d
+#       Просмотр лога вычислений: /calc_log''')
+#
+# @dp.message_handler(commands=['complex'])
+# async def complex_command(message: types.Message, command: Command):
+#     await message.reply(f" {calc_complex_main(command.args)}")
+#
+# @dp.message_handler(commands=['fraction'])
+# async def fraction_command(message: types.Message, command: Command):
+#     await message.reply(f" {calc_fraction_main(command.args)}")
+#
+# @dp.message_handler(commands=['calc_log'])
+# async def log_command(message: types.Message):
+#     await message.reply(log_reader())
 
-@dp.message_handler(commands=['calculate'])
-async def calc_command(message: types.Message):
-    await message.reply(f'''Выберите вид калькуляторов:
-      Комплексные числа: /complex вид выражения при вводе -> a+bj + c-dj
-      Дробные числа: /fraction вид выражения при вводе -> a/b + c/d 
-      Просмотр лога вычислений: /calc_log''')
+@dp.message_handler(commands=['weather','w'])
+async def choose(message: types.Message):
 
-@dp.message_handler(commands=['complex'])
-async def complex_command(message: types.Message, command: Command):
-    await message.reply(f" {calc_complex_main(command.args)}")
+    code_to_emoji = {
+        "Clear": "Ясно ☀️",
+        "Clouds": "Облачно ☁️",
+        "Rain": "Дождь ☔️",
+        "Drizzle": "Морось 🫧",
+        "Thunderstorm": "Гроза 🌩",
+        "Snow": "Снег ❄️",
+        "Mist": "Туман 🌫"
+    }
 
-@dp.message_handler(commands=['fraction'])
-async def fraction_command(message: types.Message, command: Command):
-    await message.reply(f" {calc_fraction_main(command.args)}")
+    try:
+        r = requests.get(
+            f"https://api.openweathermap.org/data/2.5/weather?q={message}&appid={WEATHER_API}&units=metric"
+        )
+        data = r.json()
 
-@dp.message_handler(commands=['calc_log'])
-async def log_command(message: types.Message):
-    await message.reply(log_reader())
+        city = data["name"]
+        cur_weather = data["main"]["temp"]
 
+        weather_description = data["weather"][0]["main"]
+        if weather_description in code_to_emoji:
+            wd = code_to_emoji[weather_description]
+        else:
+            wd = "Посмотри в окно, не могу понять что там происходит"
 
+        humidity = data["main"]["humidity"]
+        pressure = data["main"]["pressure"]
+        temp_max = data["main"]["temp_max"]
+        temp_min = data["main"]["temp_min"]
+        wind = data["wind"]["speed"]
+        sunrise_timestamp = datetime.datetime.fromtimestamp(data["sys"]["sunrise"])
+        sunset_timestamp = datetime.datetime.fromtimestamp(data["sys"]["sunset"])
+        await message.answer(f"----{datetime.datetime.now().strftime('%Y-%m-%d %H:%M')}----\n"
+                            f"Прогноз погоды в местности: {city}\nС температурой воздуха: {int(cur_weather)}°C {wd}\nВлажностью воздуха: {humidity}%\nС давлением воздуха: {pressure}мм.рт.ст"
+                            f"\nМаксимальная температура воздуха: {int(temp_max)}°C\nМинимальная температура воздуха: {int(temp_min)}°C\n"
+                            f"Скорость ветра: {wind}м/с\nВосход солнца: {sunrise_timestamp}\nЗакат солнца: {sunset_timestamp}\n"
+                            f"Хорошего дня!🤗")
 
+    except:
+
+        await message.reply("Проверьте введенное название! 🍎")
 if __name__ == '__main__':
     executor.start_polling(dp)
 
